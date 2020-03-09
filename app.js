0bd97d4b068e20a8da899bd73f1b91b7febcc577
Jacob Niedzwiecki <jake@jacob-n.com>
2020-03-09 10:47:13 -0400

    Added error hanlding & refactored

--- app.js
+++ app.js
@@ -13,24 +13,6 @@ const inquirer = require('inquirer')
 var serverURL, serverMode, apiToken
 var occasionId
 
-
-var getIPAddresses = function () {
-  var os = require("os"),
-    interfaces = os.networkInterfaces(),
-    ipAddresses = [];
-
-  for (var deviceName in interfaces) {
-    var addresses = interfaces[deviceName];
-    for (var i = 0; i < addresses.length; i++) {
-      var addressInfo = addresses[i];
-      if (addressInfo.family === "IPv4" && !addressInfo.internal) {
-        ipAddresses.push(addressInfo.address);
-      }
-    }
-  }
-  return ipAddresses;
-};
-
 var udpPort = new osc.UDPPort({
   localAddress: "0.0.0.0",
   localPort: 57121
@@ -38,6 +20,8 @@ var udpPort = new osc.UDPPort({
 
 udpPort.on("ready", async () => {
   console.log(`\n`)
+
+  // ask user to select server mode
   try {
     let answer1 = await inquirer.prompt(
       [{
@@ -52,48 +36,48 @@ udpPort.on("ready", async () => {
           name: "Offline",
           value: "offline",
           short: "offline"
-        }]
+        } ]
       }]
     )
 
     if(answer1.serverMode == "online"){
       serverMode = "online"
       serverURL = "https://new.cohort.rocks/api/v2"
-    
-      console.log(`\n`)
-      inquirer.prompt([{
-        type: "input",
-        name: "username",
-        message: "Username [register at https://new.cohort.rocks/admin if you don't have one]:",
-        default: ""
-      },{
-        type: "password",
-        name: "password",
-        message: "Password:",
-        default: ""
-      }]).then(answers => {
-        authenticate(answers.username, answers.password)
-        finishLaunch() // TODO make async
-      }).catch(error => {
+      try {
+        const credentials = await promptUsernameAndPassword()
+        apiToken = await authenticate(credentials.username, credentials.password)
+        occasionId = await promptOccasionId()
+        finishLaunch()
+      } 
+      catch (error){
         console.log(error)
-      })
-
-    } else if(answer1.serverMode == "offline"){
+      }
+    } 
+    
+    if(answer1.serverMode == "offline"){
       serverMode = "offline"
       serverURL = "http://localhost:3000/api/v2"
 
+      try {
+        const headers = getHeaders(apiToken)
+        const response = await fetch(serverURL + '/occasions/' + input + '/qrcode', { // TODO change this to a basic GET /occasions/id when that endpoint exists
+          method: 'GET',
+          headers: g
+        })
+        
+        if(response.status == 200){
+          resolve(true)
+        } else {
+          const error = await response.text()
+          reject(error)
+        }
+      }
+      catch (error){
+        console.log(error)
+      }
+
+  
       finishLaunch()
-    }
-
-    function finishLaunch (){
-      const ipAddresses = getIPAddresses();
-
-      console.log(`\nListening for OSC over UDP on IP addresses:`);
-      ipAddresses.forEach(function (address) {
-        console.log("  Host (IP):", address + ", Port:", udpPort.options.localPort);
-      });
-
-      console.log(`\nTo quit, hit Control+C`)
     }
   }
   catch (error) {
@@ -105,7 +89,7 @@ udpPort.on("ready", async () => {
       console.log(error)
     }
   }
-});
+})
 
 udpPort.on("message", function (oscMessage) {
   console.log(`\n`)
@@ -149,31 +133,147 @@ udpPort.on("message", function (oscMessage) {
   }).catch( error => {
     console.log(error)
   })
-
-  // changed to fetch for offline above // online
-  // request.post({
-  //     url: serverURL + '/occasions/1/broadcast',
-  //     headers: [{
-  //         name: 'Content-Type',
-  //         value: 'application/json'
-  //     },{
-  //         name: 'Authorization',
-  //         value: 'JWT ' + token
-  //     }],
-  //     body: payload,
-  //     json: true
-  // }, function(error, response, body){
-  //     if(error){ console.log(error) }
-  //     console.log(response.statusCode);
-  //     console.log(body);
-  // })
-});
+})
 
 udpPort.on("error", function (err) {
-    console.log(err);
-});
+    console.log(err)
+})
 
-udpPort.open();
+udpPort.open()
+
+
+function promptUsernameAndPassword(){
+  return new Promise( (resolve, reject) => {
+    console.log(`\n Enter your username and password`)
+    console.log("[register at https://new.cohort.rocks/admin if you don't have one]\n")
+    inquirer.prompt([{
+      type: "input",
+      name: "username",
+      message: "Username:",
+      default: "",
+      validate(input){
+        return new Promise( (resolve, reject) => {
+          if(input != ""){
+            resolve(true)
+          } else {
+            resolve("Error: Username cannot be blank")
+          }
+        })
+      }
+    },{
+      type: "password",
+      name: "password",
+      message: "Password:",
+      default: "",
+      validate(input){
+        return new Promise( (resolve, reject) => {
+          if(input != ""){
+            resolve(true)
+          } else {
+            resolve("Error: Password cannot be blank")
+          }
+        })
+      }
+    }]).then(answers => {
+      resolve({ username: answers.username, password: answers.password })
+    }).catch(error => {
+      console.log(error)
+      reject(error)
+    })
+  })
+}
+
+// returns an API token or an error
+authenticate = (username, password) => {
+  return new Promise( (resolve, reject) => {
+    // get a token
+    const headers = getHeaders(apiToken)
+    const payload = { username: username, password: password }
+
+    fetch(serverURL + '/login?sendToken=true', {
+      method: 'POST', 
+      body: JSON.stringify(payload),
+      headers: headers
+    }).then( response => {
+      if(response.status == 200){
+        response.json().then( results => {
+          if(results.jwt !== undefined && results.jwt){
+            console.log(`\nLogged into Cohort server`)
+            resolve(results.jwt)
+          }
+        })
+      } else {
+        // heckin error, gotta fail the promise and show the user something
+        console.log(response.status)
+        reject(error)
+      }
+    })
+  })
+}
+
+function promptOccasionId(){
+  return new Promise( (resolve, reject) => {
+    inquirer.prompt([{
+      type: "input",
+      name: "occasionId",
+      message: `\nEnter an occasion ID to broadcast to: `,
+      validate(input){
+        return new Promise( async (resolve, reject) => {
+          if(input == ""){
+            reject("Error: occasion ID cannot be blank")
+          }
+          
+          // make sure this occasion exists and user can access it
+          const headers = getHeaders(apiToken)
+          const response = await fetch(serverURL + '/occasions/' + input + '/qrcode', { // TODO change this to a basic GET /occasions/id when that endpoint exists
+            method: 'GET',
+            headers: headers
+          })
+          
+          if(response.status == 200){
+            resolve(true)
+          } else {
+            const error = await response.text()
+            reject(error)
+          }
+        })
+      }
+    }]).then( answers => {
+      console.log("\nStanding by to broadcast cues to occasion " + answers.occasionId)
+      resolve(answers.occasionId)
+    }).catch( error => {
+      reject(error)
+    }) 
+  })
+}
+
+function finishLaunch (){
+  const ipAddresses = getIPAddresses();
+
+  console.log(`\nListening for OSC over UDP on IP addresses:`);
+  ipAddresses.forEach(function (address) {
+    console.log("  Host (IP):", address + ", Port:", udpPort.options.localPort);
+  });
+
+  console.log(`\nTo quit, hit Control+C`)
+
+  function getIPAddresses(){
+    var os = require("os"),
+      interfaces = os.networkInterfaces(),
+      ipAddresses = []
+
+    for (var deviceName in interfaces) {
+      var addresses = interfaces[deviceName]
+      for (var i = 0; i < addresses.length; i++) {
+        var addressInfo = addresses[i]
+        if (addressInfo.family === "IPv4" && !addressInfo.internal) {
+          ipAddresses.push(addressInfo.address)
+        }
+      }
+    }
+    return ipAddresses
+  }
+}
 
 getHeaders = (token) => {
   let headers = {
@@ -184,55 +284,4 @@ getHeaders = (token) => {
     headers['Authorization'] = 'JWT ' + token
   }
   return headers
-}
-
-authenticate = (username, password) => {
-  // get a token
-  const headers = getHeaders(apiToken)
-  const payload = { username: username, password: password }
-
-  fetch(serverURL + '/login?sendToken=true', {
-    method: 'POST', 
-    body: JSON.stringify(payload),
-    headers: headers
-  }).then( response => {
-    if(response.status == 200){
-      response.json().then( results => {
-        if(results.jwt !== undefined && results.jwt){
-          apiToken = results.jwt
-          console.log("Logged into Cohort server")
-          requestOccasion()
-        }
-      })
-    } else {
-      // heckin error, gotta fail the promise and show the user something
-      console.log(response.status)
-    }
-  })
-}
-
-requestOccasion = () => {
-  const headers = getHeaders(apiToken)
-
-  inquirer.prompt([{
-    type: "input",
-    name: "occasionId",
-    message: `\nEnter an occasion ID to broadcast to: `
-  }]).then( answers => {
-    const userOccasionId = answers.occasionId
-    const occasionCheck = fetch(serverURL + '/occasions/' + userOccasionId + '/qrcode', { // TODO change this to a basic GET /occasions/id when that endpoint exists
-      method: 'GET',
-      headers: headers
-    }).then(response => {
-      if(response.status == 200){
-        occasionId = userOccasionId
-        console.log("Standing by to broadcast cues to occasion " + occasionId)
-      } else {
-        response.text().then( error => console.log(error))
-      }
-    })
-
-  }).catch(error=>console.log(error))
-
-  
 }
